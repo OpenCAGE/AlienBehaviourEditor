14fb23150ea2ad1d6b82588f86ed16fd48b829bc
MattFiler <matthew.filer@hotmail.co.uk>
2020-11-28 15:51:34 +0000

    Updated build info

--- BehaviourTreeTool/BehaviourTreeTool/Properties/AssemblyInfo.cs
+++ BehaviourTreeTool/BehaviourTreeTool/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BehaviourTreeTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OpenCAGE - Behaviour Tree Tool")]
+[assembly: AssemblyDescription("A tool for managing behaviour trees in Alien: Isolation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BehaviourTreeTool")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Matt Filer")]
+[assembly: AssemblyProduct("OpenCAGE - Behaviour Tree Tool")]
+[assembly: AssemblyCopyright("Copyright © Matt Filer 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
